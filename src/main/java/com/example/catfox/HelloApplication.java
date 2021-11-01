0d2fd2a6437ccb23fb88ec603ffb7cfdf4d9e465
Kasia <katarzyna.tramea@gmail.com>
2021-11-01 20:07:55 +0100

    set stage style to undecorated

--- src/main/java/com/example/catfox/HelloApplication.java
+++ src/main/java/com/example/catfox/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,6 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(fxmlLoader.load(), 520, 400);
         stage.setTitle("Welcome!");
         stage.setScene(scene);
