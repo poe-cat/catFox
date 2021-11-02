9e19c217afc7129fa04ffa93bfe3dc152b1374c5
Kasia <katarzyna.tramea@gmail.com>
2021-11-02 10:14:44 +0100

    create validateLogin()

--- src/main/java/com/example/catfox/LoginController.java
+++ src/main/java/com/example/catfox/LoginController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -23,6 +25,10 @@ public class LoginController implements Initializable {
     private ImageView brandingImageView;
     @FXML
     private ImageView lockImageView;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField enterPasswordField;
 
 
 
@@ -40,7 +46,11 @@ public class LoginController implements Initializable {
 
 
     public void loginButtonOnAction(ActionEvent event) {
-        loginMessageLabel.setText("You try to login");
+        if(usernameTextField.getText().isBlank() == false && enterPasswordField.getText().isBlank()) {
+            validateLogin();
+        } else {
+            loginMessageLabel.setText("Please enter username and password");
+        }
     }
 
 
@@ -49,5 +59,8 @@ public class LoginController implements Initializable {
         stage.close();
     }
 
+    public void validateLogin() {
+
+    }
 
 }
