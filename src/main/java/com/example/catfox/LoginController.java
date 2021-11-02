c06f12af9b25478375e2a23fe50bbd9154427387
Kasia <katarzyna.tramea@gmail.com>
2021-11-02 09:58:08 +0100

    update initialize() with images

--- src/main/java/com/example/catfox/LoginController.java
+++ src/main/java/com/example/catfox/LoginController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -15,9 +17,26 @@ public class LoginController implements Initializable {
 
     @FXML
     private Button cancelButton;
-
     @FXML
     private Label loginMessageLabel;
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView lockImageView;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        File brandingFile = new File("src/main/resources/com/example/catfox/images/unic.jpg");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+        File lockFile = new File("src/main/resources/com/example/catfox/images/heart.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
+    }
 
 
     public void loginButtonOnAction(ActionEvent event) {
@@ -30,8 +49,5 @@ public class LoginController implements Initializable {
         stage.close();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        File brandingFile = new File("images/unic.jpg");
-    }
+
 }
