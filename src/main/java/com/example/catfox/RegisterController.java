6f550c107968fffb1b8796b254997972481b4ef1
Kasia <katarzyna.tramea@gmail.com>
2021-11-02 16:11:35 +0100

    update RegisterController.java

--- src/main/java/com/example/catfox/RegisterController.java
+++ src/main/java/com/example/catfox/RegisterController.java
@@ -1,15 +1,24 @@
 package com.example.catfox;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RegisterController implements Initializable {
 
+    @FXML
+    private ImageView catImageView;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        File catFile = new File("src/main/resources/com/example/catfox/images/cat.png");
+        Image catImage = new Image(catFile.toURI().toString());
+        catImageView.setImage(catImage);
     }
 }
