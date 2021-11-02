3773282a1cfe19a7255b39049416756c5bc9bc2a
Kasia <katarzyna.tramea@gmail.com>
2021-11-02 16:22:27 +0100

    create closeButtonOnAction() with event

--- src/main/java/com/example/catfox/RegisterController.java
+++ src/main/java/com/example/catfox/RegisterController.java
@@ -1,9 +1,12 @@
 package com.example.catfox;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
@@ -13,6 +16,8 @@ public class RegisterController implements Initializable {
 
     @FXML
     private ImageView catImageView;
+    @FXML
+    private Button closeButton;
 
 
     @Override
@@ -20,5 +25,10 @@ public class RegisterController implements Initializable {
         File catFile = new File("src/main/resources/com/example/catfox/images/cat.png");
         Image catImage = new Image(catFile.toURI().toString());
         catImageView.setImage(catImage);
+    }
+
+    public void closeButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }
 }
