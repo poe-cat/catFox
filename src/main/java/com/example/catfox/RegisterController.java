3bff247f5b432e7555f281120190938993259403
Kasia <katarzyna.tramea@gmail.com>
2021-11-03 15:14:29 +0100

    add TableView field to RegisterController

--- src/main/java/com/example/catfox/RegisterController.java
+++ src/main/java/com/example/catfox/RegisterController.java
@@ -4,10 +4,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -38,6 +35,10 @@ public class RegisterController implements Initializable {
     private TextField lastNameTextField;
     @FXML
     private TextField usernameTextField;
+
+    @FXML
+    //TODO
+    private TableView tableData;
 
 
     @Override
