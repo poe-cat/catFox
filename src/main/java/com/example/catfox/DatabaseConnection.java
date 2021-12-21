2d09440fddd3f248944458716e4306affa7ec130
Kasia <katarzyna.tramea@gmail.com>
2021-12-21 13:43:20 +0100

    cosmetic changes in DatabaseConnection.java

--- src/main/java/com/example/catfox/DatabaseConnection.java
+++ src/main/java/com/example/catfox/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
 
     public Connection getConnection() {
         String databaseName = "demo_db";
-        String databaseUser = "Kasia";
+        String databaseUser = "";
         String databasePassword = "";
         String url = "jdbc:mysql://localhost/" + databaseName;
 
